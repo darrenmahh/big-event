6c37aa426c293ce140a2d5b3b711d1689c1882ec
machong <machong1009@gmail.com>
2024-09-18 22:33:25 +0800

    fetal(upload):first commit

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,30 @@
+package org.example.controller;
+
+import org.example.pojo.Result;
+import org.example.pojo.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Result register(String username,String password){
+        System.out.println("@PostMapping(\"/register\")");
+        User u = userService.findByUserName(username);
+
+        if (u == null){
+            userService.register(username,password);
+            return Result.success();
+        }else {
+            return Result.error("用户名已被占用");
+        }
+    }
+}
---
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package org.example.service.impl;
+
+import org.example.mapper.UserMapper;
+import org.example.pojo.User;
+import org.example.service.UserService;
+import org.example.utils.Md5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User findByUserName(String username) {
+        User u = userMapper.findByUserName(username);
+        return u;
+    }
+
+    @Override
+    public void register(String username, String password) {
+        // 加密
+        String md5String = Md5Util.getMD5String(password);
+        // 添加
+        userMapper.add(username,md5String);
+    }
+}
